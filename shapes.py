0bbcbbb24998faa36a52ccb95c876d54f999fe14
Nathan <bro19053@byui.edu>
2024-05-08 12:04:09 -0600

    added triangle

--- shapes.py
+++ shapes.py
@@ -6,10 +6,17 @@ def calculate_rectangle_area(width, height):
     area = width * height
     return area
 
+def calc_tri_area(h, b):
+    area = 1 / 2 * h * b
+    return area
+
 def main():
     print('This is our shapes program')
 
     rectangle_area = calculate_rectangle_area(10, 20)
     print(f'The area of the rectangle is: {rectangle_area}')
+
+    tri_area = calc_tri_area(20, 79)
+    print(f'The area of the triangle is: {tri_area}')
 
 main()
